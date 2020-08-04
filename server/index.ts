95f478fb17449364e34b3f24923b887c0acf92cc
Itamar Tati <itamar.tati1994@gmail.com>
2020-08-04 09:25:07 +0100

    added server files and connected to the client

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+declare var path: any
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+app.listen(process.env.PORT || 4000, () => console.log('All is running'))
