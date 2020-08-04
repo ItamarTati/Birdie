924dca5eae8fd1f54af989d1f480a499df44201f
Itamar Tati <itamar.tati1994@gmail.com>
2020-08-04 12:35:14 +0100

    finished server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect}  from 'react';
 import './App.css';
 
+
 function App() {
+  const [data, setData] = useState([])
+  const url: string = 'http://localhost:4000/events'
+  useEffect(() => {
+    fetch(url) 
+    .then(response => response.json())
+    .then(shows => setData(shows))
+    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+}, []);
+console.log(data)  
+//@ts-ignore
+  console.log(data.map((obj) => obj.id))
+    //@ts-ignore
+    console.log(data.filter((obj) => obj.event_type === 'mood_observation' ))
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express()
+const mysql = require('mysql'); 
+const cors = require('cors');
 
+app.use(cors());
+
+const connection = mysql.createConnection({
+    host: 'birdie-test.cyosireearno.eu-west-2.rds.amazonaws.com',
+    port: '3306',
+    user: 'test-read',
+    password: 'xnxPp6QfZbCYkY8',
+    database: 'birdietest',
+});
+app.get('/events', (req, res) => {
+    connection.connect();
+    connection.query(`SELECT id, event_type, visit_id, timestamp, caregiver_id, care_recipient_id, payload from events`, function(err, rows, fields) {
+        if (!err) {
+            res.send(JSON.stringify(rows));
+        } else {
+            console.log('Error while performing Query.');
+        }
+    });
+    connection.end();
+});
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
